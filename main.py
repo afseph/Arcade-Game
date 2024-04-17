c54036926b5197d868a9e2294967583e5e3bcfab
afseph <afseph@gmail.com>
2024-04-17 23:12:55 +0300

    init commit

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ class Game():
     def __init__(self):
         pass
 
-    def startGame(self):
-        pass
-
-    def endGame(self):
-        pass
-
-
-    def ggg(self):
-        pass
-
-    def fff(self):
-        pass
+    
 if __name__ == "__main__":
     g = Game()
