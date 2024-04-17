abb4de80c9df59ce3615fb58205148248bd60ffe
Bomix666 <knikita096@gmail.com>
2024-04-18 01:23:18 +0300

    add class: Enemy

---
+++ enemy.py
@@ -0,0 +1,12 @@
+class Enemy:
+    def __init__(self, attack:int, defence:int, hp:int, name:str) -> None:
+        """
+            attack: Урон
+            defence: Защита,
+            hp: Жизни,
+            name: Название персонажа,
+        """
+        self.attack = attack
+        self.defence = defence
+        self.hp = hp
+        self.name = name
