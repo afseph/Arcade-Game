12e764f6b69a2c473cf6ffbd8ca3d047ba99462e
afseph <afseph@gmail.com>
2024-04-18 00:12:22 +0300

    added main hero class

--- heroes.py
+++ heroes.py
@@ -1,3 +1,57 @@
 class Hero:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, attack:int, defence:int, hp:int, name:str, f_id:int) -> None:
+        """
+            attack: Урон
+            defence: Защита,
+            hp: Жизни,
+            name: Название персонажа,
+            f_id: Id фракции
+        """
+        self.attack = attack
+        self.defence = defence
+        self.hp = hp
+        self.name = name
+        self.f_id = f_id
+
+        # ! Выдача бафаов в зависимости от фракции
+        if self.fraction_buff[f_id] == "hp":
+            self.hp *= 1.25
+        elif self.fraction_buff[f_id] == "defence":
+            self.defence *= 1.25
+        elif self.fraction_buff[f_id] == "attack":
+            self.attack *= 1.25
+
+    # Бафы фракций по Id
+    fraction_buff = {
+        1: "hp",
+        2: "attack",
+        3: "defence"
+    }
+
+    # Названия фракций по Id
+    fractions = {
+        1:"Люди",
+        2:"Эльфы",
+        3:"Дракониды"
+    }
+
+    def get_fraction(self):
+        """
+            Получение названия фракции героя
+        """
+        return self.fractions[self.f_id]
+
+    def print_stats(self):
+        """
+            Вывод статистики в консоль
+        """
+        print(str(self.name) + ":" + str(self.attack), self.defence, self.hp, self.get_fraction())
+
+
+class Archer(Hero):
+    def __init__(self, attack: int, defence: int, hp: int, name: str, f_id:int) -> None:
+        super().__init__(attack, defence, hp, name, f_id)
+
+
+a = Archer(attack=10, defence=20, hp=100, name="Друид", f_id=2)
+a.print_stats()
